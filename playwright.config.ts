a3611047a1975d30c82bb91cf26915c1cb66ff6b
Anderson Cahet <Anderson.cahet@docplanner.com>
2025-09-27 17:56:40 +0200

    chore: refactor articles test
    - create const to endpoints and http status code
    - update playwright config to avoid tests to run twice

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,9 +16,6 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -31,10 +28,10 @@ export default defineConfig({
     ['list'] // Change to your desired report directory
     // Add other reporters if needed
   ],
-  
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    
+
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
   },
@@ -44,17 +41,19 @@ export default defineConfig({
     {
       name: 'ui-tests',
       use: { ...devices['Desktop Chrome'] },
-      testDir:'./tests/ui-tests',
+      testDir: './tests/ui-tests',
       outputDir: './tests/report/test-results/ui-tests',
+      fullyParallel: true,
     },
     /**
      * uncomment below if you want to have an PW-API project
      */
     {
       name: 'api-tests',
-      testDir:'./tests/api-tests',
+      testDir: './tests/api-tests',
       workers: 1,
       outputDir: './tests/report/test-results/api-tests',
+      fullyParallel: false,
     },
 
   ],
