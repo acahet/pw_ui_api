46872e7c795300bb5aa05494ba253cd26424fa6a
Anderson Cahet <Anderson.cahet@docplanner.com>
2025-09-27 16:24:00 +0200

    chore: Update playwright.yml (#4)
    * Update playwright.yml
    
    * chore: add api tests to workflow
    
    * update to use only chromium
    
    - add .env to gitignore

--- tests/api-tests/articles.spec.ts
+++ tests/api-tests/articles.spec.ts
@@ -4,16 +4,22 @@ import { APIResponse, expect } from '@playwright/test';
 const domain = 'https://conduit-api.bondaracademy.com/';
 const articlesPath = 'api/articles';
 const articlesParam = '?limit=10&offset=0';
+const loginPath = '/api/users/login';
 
+let authToken: string;
 test.describe('API tests', () => {
-  test('Get all articles', async ({ request }) => {
-    const articlesResponse: APIResponse = await request.get(
-      `${domain}/${articlesPath}${articlesParam}`,
-    );
-    expect(articlesResponse.status()).toBe(200);
-    const articlesResponseBody = await articlesResponse.json();
-    expect(articlesResponseBody).toHaveProperty('articles');
-    expect(articlesResponseBody).toHaveProperty('articlesCount');
-    expect(articlesResponseBody.articles.length).toBeGreaterThan(0);
-  });
+    test('GET Articles', async ({ request }) => {
+        const articlesResponse: APIResponse = await request.get(
+            `${domain}/${articlesPath}${articlesParam}`,
+        );
+        expect(articlesResponse.status()).toBe(200);
+        const articlesResponseBody = await articlesResponse.json();
+        expect(articlesResponseBody).toHaveProperty('articles');
+        expect(articlesResponseBody).toHaveProperty('articlesCount');
+        expect(articlesResponseBody.articles.length).toBeGreaterThan(0);
+        expect(articlesResponseBody.articles.length).toBeLessThanOrEqual(10);
+    });
+
+
 });
+
