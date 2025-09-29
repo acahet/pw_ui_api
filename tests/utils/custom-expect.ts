4febd7cd64482c5260656f36108c8f37433d8825
Anderson Cahet <Anderson.cahet@docplanner.com>
2025-09-29 17:44:11 +0200

    chore: schema assertion
    create custom expect to assert schema

--- tests/utils/custom-expect.ts
+++ tests/utils/custom-expect.ts
@@ -1,4 +1,6 @@
 import { APILogger } from './logger';
+import { validateSchema } from './schema-validator';
+import { SchemaDir, SchemaFile } from '@config';
 import { expect as baseExpect } from '@playwright/test';
 
 let apiLogger: APILogger;
@@ -12,11 +14,22 @@ declare global {
     interface Matchers<R, T> {
       shouldBeEqual(expected: T): R;
       shouldBeLessThanOrEqual(expected: T): R;
+      shouldMatchSchema<Dir extends SchemaDir, File extends SchemaFile<Dir>>(
+        dirName: Dir,
+        fileName: File,
+        createSchemaFlag?: boolean,
+      ): Promise<R>;
     }
   }
 }
 
 export const expect = baseExpect.extend({
+  /**
+   * Custom matcher to compare two values for equality with enhanced logging.
+   * @param received - The received value
+   * @param expected - The expected value
+   * @returns The result of the comparison
+   */
   shouldBeEqual(received: any, expected: any) {
     let pass: boolean;
     let logs: string = '';
@@ -42,6 +55,12 @@ export const expect = baseExpect.extend({
       `Recent API Activity: \n${logs}`;
     return { message: () => message, pass };
   },
+  /**
+   * Custom matcher to compare two values for less than or equal with enhanced logging.
+   * @param received - The received value
+   * @param expected - The expected value
+   * @returns The result of the comparison
+   */
   shouldBeLessThanOrEqual(received: any, expected: any) {
     let pass: boolean;
     let logs: string = '';
@@ -65,6 +84,34 @@ export const expect = baseExpect.extend({
       `Expected: ${hint}${this.utils.printExpected(expected)}\n` +
       `Received: ${this.utils.printReceived(received)}\n\n` +
       `Recent API Activity: \n${logs}`;
+    return { message: () => message, pass };
+  },
+  /**
+   * Custom matcher to validate the schema of a received value.
+   * @param received - The received value
+   * @param dirName - The directory name for the schema
+   * @param fileName - The file name for the schema
+   * @param createSchemaFlag - Flag to create a new schema if it doesn't exist
+   * @returns The result of the schema validation
+   */
+  async shouldMatchSchema<Dir extends SchemaDir, File extends SchemaFile<Dir>>(
+    received: any,
+    dirName: Dir,
+    fileName: File,
+    createSchemaFlag: boolean = false,
+  ) {
+    let pass: boolean;
+    let message: string = '';
+
+    try {
+      await validateSchema(dirName, fileName, received, createSchemaFlag);
+      pass = true;
+      message = 'Schema validation passed';
+    } catch (e: any) {
+      pass = false;
+      const logs = apiLogger.getRecentLogs();
+      message = `${e.message}\n\n\RecentAPI Activity:\n${logs}`;
+    }
     return { message: () => message, pass };
   },
 });
