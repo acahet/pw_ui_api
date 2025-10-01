51eea7a4d39ca16618600728d4073aad51f55f01
Anderson Cahet <Anderson.cahet@docplanner.com>
2025-10-01 12:18:40 +0200

    chore: implement ui test (#18)
    - use same website for ui
    - use po

--- tests/api-tests/user/articles/user_articles.spec.ts
+++ tests/api-tests/user/articles/user_articles.spec.ts
@@ -3,55 +3,55 @@ import { expect } from '@utils/custom-expect';
 import { RequestHandler } from '@utils/request-handler';
 
 test.describe(
-    'Feature: User Articles API',
-    {
-        annotation: {
-            type: 'api-user-articles',
-            description: 'Tests for the Logged user Articles API endpoints',
-        },
-        tag: ['@user', '@articles'],
+  'Feature: User Articles API',
+  {
+    annotation: {
+      type: 'api-user-articles',
+      description: 'Tests for the Logged user Articles API endpoints',
     },
-    () => {
-        test('GET Current User Favorite Articles', async ({
-            api,
-            endpoints,
-            httpStatus: { Status200_Ok },
-        }) => {
-            const currentUser = await api
-                .path(endpoints.user)
-                .getRequest(Status200_Ok);
+    tag: ['@user', '@articles'],
+  },
+  () => {
+    test('GET Current User Favorite Articles', async ({
+      api,
+      endpoints,
+      httpStatus: { Status200_Ok },
+    }) => {
+      const currentUser = await api
+        .path(endpoints.user)
+        .getRequest(Status200_Ok);
 
-            const articlesResponse = await api
-                .path(endpoints.articles)
-                .clearAuth()
-                .params({ favorited: currentUser.user.username, limit: 10, offset: 0 })
+      const articlesResponse = await api
+        .path(endpoints.articles)
+        .clearAuth()
+        .params({ favorited: currentUser.user.username, limit: 10, offset: 0 })
 
-                .getRequest(Status200_Ok);
-            await expect(articlesResponse).shouldMatchSchema(
-                'articles',
-                'GET_articles_favorite',
-            );
-            expect(articlesResponse.articlesCount).shouldBeEqual(0);
-        });
+        .getRequest(Status200_Ok);
+      await expect(articlesResponse).shouldMatchSchema(
+        'articles',
+        'GET_articles_favorite',
+      );
+      expect(articlesResponse.articlesCount).shouldBeEqual(0);
+    });
 
-        test('GET Current User Articles', async ({
-            api,
-            endpoints,
-            httpStatus: { Status200_Ok },
-        }) => {
-            const currentUser = await api
-                .path(endpoints.user)
-                .getRequest(Status200_Ok);
+    test('GET Current User Articles', async ({
+      api,
+      endpoints,
+      httpStatus: { Status200_Ok },
+    }) => {
+      const currentUser = await api
+        .path(endpoints.user)
+        .getRequest(Status200_Ok);
 
-            const articlesResponse = await api
-                .path(endpoints.articles)
-                .params({ author: currentUser.user.username, limit: 10, offset: 0 })
-                .getRequest(Status200_Ok);
-            await expect(articlesResponse).shouldMatchSchema(
-                'articles',
-                'GET_user_articles',
-            );
-            expect(articlesResponse.articlesCount).shouldBeEqual(0);
-        });
-    },
+      const articlesResponse = await api
+        .path(endpoints.articles)
+        .params({ author: currentUser.user.username, limit: 10, offset: 0 })
+        .getRequest(Status200_Ok);
+      await expect(articlesResponse).shouldMatchSchema(
+        'articles',
+        'GET_user_articles',
+      );
+      expect(articlesResponse.articlesCount).shouldBeEqual(0);
+    });
+  },
 );
--- tests/ui-tests/pages/Homepage/index.ts
+++ tests/ui-tests/pages/Homepage/index.ts
@@ -1,7 +1,30 @@
 import { Page } from '@playwright/test';
 
-export class Homepage {
-  constructor(private readonly page: Page) {
+class Selectors {
+  navBar = 'ul[class*="navbar-nav"] li';
+  loginBtn = 'a[routerlink="/login"]';
+}
+
+class Actions {
+  constructor(
+    private readonly page: Page,
+    private selectors: Selectors,
+  ) {
     this.page = page;
+    this.selectors = selectors;
+  }
+
+  async goToLoginPage(): Promise<void> {
+    await this.page.locator(this.selectors.loginBtn).click();
+  }
+  async clickLoginBtn(): Promise<void> {
+    await this.page.locator(this.selectors.loginBtn).click();
+  }
+}
+export class HomePage {
+  readonly selectors = new Selectors();
+  readonly do: Actions;
+  constructor(private readonly page: Page) {
+    this.do = new Actions(this.page, this.selectors);
   }
 }
