0c031b084b067a0aa176ec9125122afc0524ff0a
Anderson Cahet <Anderson.cahet@docplanner.com>
2025-10-01 13:05:10 +0200

    Update login.spec.ts

--- tests/ui-tests/feature/login/login.spec.ts
+++ tests/ui-tests/feature/login/login.spec.ts
@@ -19,6 +19,6 @@ test('login', async ({ page, loginPage, homePage }) => {
   });
 
   await test.step('Then I navigate back to home page with signed in nav bar', async () => {
-    await expect(page.locator(homePage.selectors.navBar)).toHaveCount(3);
+    await expect(page.locator(homePage.selectors.navBar)).toHaveCount(0);
   });
 });
