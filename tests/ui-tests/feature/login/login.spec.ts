bd12db74cb83f5a1d66a768bfcf1839dcca3316d
andersoncahet <anderson.cahet@gmail.com>
2025-12-16 23:14:27 +0100

    style: format code

--- tests/ui-tests/feature/login/login.spec.ts
+++ tests/ui-tests/feature/login/login.spec.ts
@@ -1,21 +1,21 @@
-import { test } from '@fixtures';
-import { expect } from '@playwright/test';
+import { test } from "@fixtures";
+import { expect } from "@playwright/test";
 
-test('login', async ({ page, loginPage, homePage }) => {
-	await test.step('GIVEN - I am at the login page', async () => {
-		await page.goto('/');
+test("login", async ({ page, loginPage, homePage }) => {
+	await test.step("GIVEN - I am at the login page", async () => {
+		await page.goto("/");
 		await expect(page.locator(homePage.selectors.navBar)).toHaveCount(3);
 		await homePage.do.goToLoginPage();
 	});
-	await test.step('WHEN - I add valid credentials', async () =>
+	await test.step("WHEN - I add valid credentials", async () =>
 		await loginPage.do.compileLoginForm(
 			process.env.EMAIL_API as string,
-			process.env.PASSWORD_API as string
+			process.env.PASSWORD_API as string,
 		));
 
-	await test.step('AND - I click signin button', async () =>
+	await test.step("AND - I click signin button", async () =>
 		await loginPage.do.signIn());
 
-	await test.step('Then I navigate back to home page with signed in nav bar', async () =>
+	await test.step("Then I navigate back to home page with signed in nav bar", async () =>
 		await expect(page.locator(homePage.selectors.navBar)).toHaveCount(3));
 });
