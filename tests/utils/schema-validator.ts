9cfb73ce89cf3c568078983b98040e2a4932b982
andersoncahet <anderson.cahet@gmail.com>
2025-12-17 15:36:49 +0100

    Update schema-validator.ts

--- tests/utils/schema-validator.ts
+++ tests/utils/schema-validator.ts
@@ -21,7 +21,7 @@ export async function validateSchema<
 	dirName: Dir,
 	fileName: File,
 	responseBody: object,
-	createSchemaFlag: boolean = false,
+	createSchemaFlag: boolean = true,
 ): Promise<void> {
 	const schemaPath = path.join(
 		SCHEMA_BASE_PATH,
